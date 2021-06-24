34b3bf7dab16fb3e6a9c2704cfc9164601d19306
songrit maleerat <song.cpe@gmail.com>
2021-06-24 12:17:44 +0700

    Initial project

---
+++ src/main/kotlin/com/song/cpe/kotlinasynchonousflowplaygroud/KotlinAsynchonousFlowPlaygroudApplication.kt
@@ -0,0 +1,11 @@
+package com.song.cpe.kotlinasynchonousflowplaygroud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinAsynchonousFlowPlaygroudApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinAsynchonousFlowPlaygroudApplication>(*args)
+}
---
+++ src/test/kotlin/com/song/cpe/kotlinasynchonousflowplaygroud/KotlinAsynchonousFlowPlaygroudApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.song.cpe.kotlinasynchonousflowplaygroud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinAsynchonousFlowPlaygroudApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
